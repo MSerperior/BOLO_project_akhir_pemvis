8a36d1814c2b63c2976c71664cdd0d3fb60d7250
MSerperior <45038103+MSerperior@users.noreply.github.com>
2021-12-06 15:35:22 +0700

    Initial commit

---
+++ src/bolo_project_akhir_pemvis/BOLO_project_akhir_pemvis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bolo_project_akhir_pemvis;
+
+/**
+ *
+ * @author 62821
+ */
+public class BOLO_project_akhir_pemvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
