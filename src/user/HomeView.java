b3a8dd9ce8432ba2bc097a591396542cdb4087f0
MSerperior <45038103+MSerperior@users.noreply.github.com>
2022-01-14 22:28:53 +0700

    hapus chart dashboard

--- src/user/HomeView.java
+++ src/user/HomeView.java
@@ -25,19 +25,7 @@ public class HomeView extends javax.swing.JFrame {
      * Creates new form HomeView
      */
     public HomeView() {
-        // Create dataset  
-        DefaultCategoryDataset dataset = createDataset();
-        // Create chart  
-        JFreeChart chart = ChartFactory.createLineChart(
-                "Site Traffic", // Chart title  
-                "Date", // X-Axis Label  
-                "Number of Visitor", // Y-Axis Label  
-                dataset
-        );
-        ChartPanel panel = new ChartPanel(chart);
-        panel.setSize(500,400);
         initComponents();
-        PanelDashboard.add(panel);
         setKomponen();
         setTampilan();
     }
@@ -876,32 +864,6 @@ public class HomeView extends javax.swing.JFrame {
                 arrPanel[i].setVisible(true);
             }
         }
-    }
-
-    private DefaultCategoryDataset createDataset() {
-
-        String series1 = "Visitor";
-        String series2 = "Unique Visitor";
-
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-
-        dataset.addValue(200, series1, "2016-12-19");
-        dataset.addValue(150, series1, "2016-12-20");
-        dataset.addValue(100, series1, "2016-12-21");
-        dataset.addValue(210, series1, "2016-12-22");
-        dataset.addValue(240, series1, "2016-12-23");
-        dataset.addValue(195, series1, "2016-12-24");
-        dataset.addValue(245, series1, "2016-12-25");
-
-        dataset.addValue(150, series2, "2016-12-19");
-        dataset.addValue(130, series2, "2016-12-20");
-        dataset.addValue(95, series2, "2016-12-21");
-        dataset.addValue(195, series2, "2016-12-22");
-        dataset.addValue(200, series2, "2016-12-23");
-        dataset.addValue(180, series2, "2016-12-24");
-        dataset.addValue(230, series2, "2016-12-25");
-
-        return dataset;
     }
 
     /**
