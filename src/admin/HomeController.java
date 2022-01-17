f8447525a5d86f801c4427de7af37713e2e99ea0
MSerperior <45038103+MSerperior@users.noreply.github.com>
2022-01-17 19:45:29 +0700

    urutan top up ascending, insert ke riwayat top up

--- src/admin/HomeController.java
+++ src/admin/HomeController.java
@@ -24,10 +24,10 @@ public class HomeController {
     public ResultSet getRental(int r) throws SQLException{
         if(r == 0){
             
-            return dbConn.stm.executeQuery("SELECT *,total_pembayaran pendapatan FROM rental ORDER BY waktu_pemesanan DESC LIMIT 14");
+            return dbConn.stm.executeQuery("SELECT *,total_pembayaran pendapatan FROM rental ORDER BY waktu_pemesanan ASC LIMIT 14");
         }
         else if(r == 1){
-            return dbConn.stm.executeQuery("SELECT YEAR(waktu_pemesanan) tahun,MONTH(waktu_pemesanan) bulan,SUM(total_pembayaran) pendapatan FROM rental GROUP BY 1,2 ORDER BY waktu_pemesanan DESC LIMIT 12");
+            return dbConn.stm.executeQuery("SELECT YEAR(waktu_pemesanan) tahun,MONTH(waktu_pemesanan) bulan,SUM(total_pembayaran) pendapatan FROM rental GROUP BY 1,2 ORDER BY waktu_pemesanan ASC LIMIT 12");
         }
         else{
             return null;
@@ -40,10 +40,10 @@ public class HomeController {
      */
     public ResultSet getRiwayatTopUp(int r) throws SQLException{
         if(r == 0){
-            return dbConn.stm.executeQuery("SELECT *, jumlah pendapatan FROM riwayat_top_up ORDER BY tanggal_top_up DESC LIMIT 14");
+            return dbConn.stm.executeQuery("SELECT *, jumlah pendapatan FROM riwayat_top_up ORDER BY tanggal_top_up ASC LIMIT 14");
         }
         else if(r == 1){
-            return dbConn.stm.executeQuery("SELECT YEAR(tanggal_top_up) tahun,MONTH(tanggal_top_up) bulan,SUM(jumlah) pendapatan FROM riwayat_top_up GROUP BY 1,2 ORDER BY tanggal_top_up DESC LIMIT 12");
+            return dbConn.stm.executeQuery("SELECT YEAR(tanggal_top_up) tahun,MONTH(tanggal_top_up) bulan,SUM(jumlah) pendapatan FROM riwayat_top_up GROUP BY 1,2 ORDER BY tanggal_top_up ASC LIMIT 12");
         }
         else{
             System.out.println("admin.HomeController.getRiwayatTopUp()");
@@ -55,8 +55,9 @@ public class HomeController {
         return dbConn.stm.executeQuery("SELECT * FROM jenis_lapangan");
     }
     
-    public void topUp(String user_id, int nominal) throws SQLException{
-        dbConn.stm.execute(String.format("UPDATE user SET saldo=saldo+%d WHERE id_user=%s",nominal,user_id));
+    public void topUp(String id_user, int nominal) throws SQLException{
+        dbConn.stm.execute(String.format("UPDATE user SET saldo=saldo+%d WHERE id_user=%s",nominal,id_user));
+        dbConn.stm.execute(String.format("INSERT INTO riwayat_top_up(id_user, id_admin, jumlah, tanggal_top_up) VALUES(%d, $d, %d, NOW())", id_user, 1002, nominal));
     }
     
 }
